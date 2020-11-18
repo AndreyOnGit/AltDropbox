c9231a0a1e34ea74b85c300264b7a23cd94769f5
AndreyOnGit <lacosh@gmail.com>
2020-11-18 23:12:37 +0300

    added changing theme, corrected work with renaming files and etc.

---
+++ client/src/main/java/RenamingController.java
@@ -0,0 +1,41 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+public class RenamingController {
+    private Controller controller;
+    private String panel;
+    private String oldName;
+
+    @FXML
+    TextField fieldName;
+
+    public void setInfo(Controller controller, String panel, String oldName) {
+        this.controller = controller;
+        this.panel = panel;
+        this.oldName = oldName;
+    }
+
+    public void btnChangeName(ActionEvent event) {
+        if (panel.equals("left")) {
+            File file = new File(controller.leftPC.getCurrentPath(), oldName);
+            File newFile = new File(controller.leftPC.getCurrentPath(), fieldName.getText());
+            //todo сделать проверку на некорректный ввод
+            file.renameTo(newFile);
+            controller.leftPC.updateLeftList(Paths.get(controller.leftPC.getCurrentPath()));
+        }
+        if (panel.equals("right")) {
+            controller.send("rename");
+            controller.send(controller.rightPC.pathField.getText() + "/" + oldName);
+            //todo сделать проверку на некорректный ввод
+            controller.send(fieldName.getText());
+            controller.rightPC.updateTable(controller.rightPC.pathField.getText());
+        }
+        panel = "";
+        controller.closeRenameForm();
+    }
+
+}
